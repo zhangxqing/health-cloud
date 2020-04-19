928b69c97ecc5019360008bb4722dab3546e6379
zq <846455480@qq.com>
2020-04-19 21:48:41 +0800

    修改网关不能启动问题,登陆界面验证码配置，路由配置

--- health-gateway/src/main/java/com/health/gateway/HealthGatewayApplication.java
+++ health-gateway/src/main/java/com/health/gateway/HealthGatewayApplication.java
@@ -2,8 +2,11 @@ package com.health.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class HealthGatewayApplication {
 
     public static void main(String[] args) {
