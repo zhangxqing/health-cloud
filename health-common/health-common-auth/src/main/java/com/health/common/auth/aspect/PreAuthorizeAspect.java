6682c3c20ac273ade5cb19829bdf6d48a638b5b3
zq <846455480@qq.com>
2020-04-22 12:46:36 +0800

    fix bug 代码格式化,父模块打包提示未指定未pom问题

--- health-common/health-common-auth/src/main/java/com/health/common/auth/aspect/PreAuthorizeAspect.java
+++ health-common/health-common-auth/src/main/java/com/health/common/auth/aspect/PreAuthorizeAspect.java
@@ -21,44 +21,35 @@ import java.util.Optional;
 @Aspect
 @Component
 @Slf4j
-public class PreAuthorizeAspect
-{
+public class PreAuthorizeAspect {
     @Autowired
     private RemoteMenuService sysMenuClient;
 
     @Around("@annotation(com.health.common.auth.annotation.HasPermissions)")
-    public Object around(ProceedingJoinPoint point) throws Throwable
-    {
+    public Object around(ProceedingJoinPoint point) throws Throwable {
         Signature signature = point.getSignature();
         MethodSignature methodSignature = (MethodSignature) signature;
         Method method = methodSignature.getMethod();
         HasPermissions annotation = method.getAnnotation(HasPermissions.class);
-        if (annotation == null)
-        {
+        if (annotation == null) {
             return point.proceed();
         }
         String authority = new StringBuilder(annotation.value()).toString();
-        if (has(authority))
-        {
+        if (has(authority)) {
             return point.proceed();
-        }
-        else
-        {
+        } else {
             throw new ForbiddenException();
         }
     }
 
-    private boolean has(String authority)
-    {
+    private boolean has(String authority) {
         // 用超管帐号方便测试，拥有所有权限
         HttpServletRequest request = ServletUtils.getRequest();
         String tmpUserKey = request.getHeader(Constants.CURRENT_ID);
-        if (Optional.ofNullable(tmpUserKey).isPresent())
-        {
+        if (Optional.ofNullable(tmpUserKey).isPresent()) {
             Long userId = Long.valueOf(tmpUserKey);
             log.debug("userId:{}", userId);
-            if (userId == 1L)
-            {
+            if (userId == 1L) {
                 return true;
             }
             return sysMenuClient.selectPermsByUserId(userId).stream().anyMatch(authority::equals);
