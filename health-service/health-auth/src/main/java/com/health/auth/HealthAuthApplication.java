0e3605e112ce61dc089c8bc1b60a72d469ccb9a4
zq <846455480@qq.com>
2020-05-26 16:06:22 +0800

    注册中心 配置中心改为nacos

--- health-service/health-auth/src/main/java/com/health/auth/HealthAuthApplication.java
+++ health-service/health-auth/src/main/java/com/health/auth/HealthAuthApplication.java
@@ -4,14 +4,14 @@ import com.health.common.annotation.EnableHyFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author zq
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHyFeignClients
 @ComponentScan(value = {"com.health.*"})
 public class HealthAuthApplication {
--- health-service/health-system/src/main/java/com/health/system/HealthSystemApplication.java
+++ health-service/health-system/src/main/java/com/health/system/HealthSystemApplication.java
@@ -5,17 +5,17 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author zq
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableEurekaClient
 @EnableHyFeignClients
 @MapperScan("com.health.*.mapper")
 @ComponentScan(value = {"com.health.*"})
+@EnableDiscoveryClient
 public class HealthSystemApplication {
 
     public static void main(String[] args) {
